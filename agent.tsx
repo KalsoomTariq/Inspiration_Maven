046feb1ccaadc1249126253e5560b20ede5acb98
Kalsoom Tariq <kalsoomtariq191@gmail.com>
2024-11-17 13:51:11 +0500

    Added Sentiment Analysis Action

--- agent.tsx
+++ agent.tsx
@@ -29,6 +29,7 @@ const InspirationMaven = () => {
  
   return (
     <>
+    {/* Action To Fetch Books based on the subject user is interested in */}
       <Action
       name='fetchBooksByCategory'
       description="Retrieve a list books by category"
@@ -58,6 +59,37 @@ const InspirationMaven = () => {
           } 
       } 
       /> 
+      {/* Action To Gauge User's Life Aspirations */}
+    <Action
+        name='gaugeUserAspirations'
+        description="A personality and aspiration analysis of the user"
+        schema={ 
+            z.object({ 
+            prompt: z.string(), 
+            }) 
+        } 
+        examples={[ 
+            {  
+              prompt: 'I\'m feeling uncertain about my career path. I\’ve always been interested in art, but I also have a strong analytical side that draws me toward technology. I want to combine my love for creativity with my skills in problem-solving. ', 
+            }, 
+            {  
+              prompt: 'I\’m trying to focus on self-improvement, particularly in terms of building resilience and understanding my emotions. I\’m looking for resources that could guide me in developing better emotional intelligence and handling difficult situations more effectively.', 
+            }, 
+        ]} 
+        handler={ 
+            (e: PendingActionEvent) => { 
+            const { prompt } = e.data.message.args as { prompt: string }; 
+            const monologueString = `\ 
+              1. Analyzes User's Prompt to gauge user's life goals and inspirations.
+                  a. If prompt does not contain any useful information, gently ask the user questions about their life goals and choices.
+                  b. If prompt contains useful information, analyze the user's goals, aspirations, and current situation.
+              2. Ask the user if they want a book recommendation that will help them in achieving their goals.
+            ` + '\n\n <prompt>' + prompt + '</prompt>'; 
+            e.data.agent.monologue(monologueString);   
+            e.commit(); 
+            } 
+        } 
+        /> 
     </>
   );
 }
